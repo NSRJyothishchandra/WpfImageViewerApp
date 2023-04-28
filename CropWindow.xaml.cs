4b156cf62902f613fc1dc9b2b5258e49e256efa0
jyothishn <jyothishn@TFLT42>
2023-04-27 23:06:38 -0500

    Add project files.

---
+++ CropWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfImageViewerApp
+{
+    public partial class CropWindow : Window
+    {
+        private BitmapImage bitmap;
+        private double startX, startY, endX, endY;
+        private bool isDragging = false;
+        private BitmapSource bitmapSource;
+
+        public CroppedBitmap CroppedImage { get; private set; }
+
+        public CropWindow(BitmapImage bitmap)
+        {
+            InitializeComponent();
+            this.bitmap = bitmap;
+            image.Source = bitmap;
+        }
+
+        public CropWindow(BitmapSource bitmapSource)
+        {
+            this.bitmapSource = bitmapSource;
+        }
+
+        public CroppedBitmap GetCroppedImage()
+        {
+            if (selection.Visibility == Visibility.Visible)
+            {
+                
+                double x = Canvas.GetLeft(selection);
+                double y = Canvas.GetTop(selection);
+                double width = selection.ActualWidth;
+                double height = selection.ActualHeight;
+
+                // Convert the selection dimensions to pixel values
+                double dpiScale = bitmap.DpiX / 96.0;
+                x *= dpiScale;
+                y *= dpiScale;
+                width *= dpiScale;
+                height *= dpiScale;
+
+                // Create and return the cropped bitmap
+                return new CroppedBitmap(bitmap, new Int32Rect((int)x, (int)y, (int)width, (int)height));
+            }
+            else
+            {
+                // Return the original bitmap
+                return new CroppedBitmap(bitmap, new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
+            }
+        }
+
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                startX = e.GetPosition(canvas).X;
+                startY = e.GetPosition(canvas).Y;
+                endX = startX;
+                endY = startY;
+                Canvas.SetLeft(selection, startX);
+                Canvas.SetTop(selection, startY);
+                selection.Width = 0;
+                selection.Height = 0;
+                selection.Visibility = Visibility.Visible;
+                isDragging = true;
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                endX = e.GetPosition(canvas).X;
+                endY = e.GetPosition(canvas).Y;
+                double x = startX < endX ? startX : endX;
+                double y = startY < endY ? startY : endY;
+                double width = startX < endX ? endX - startX : startX - endX;
+                double height = startY < endY ? endY - startY : startY - endY;
+                Canvas.SetLeft(selection, x);
+                Canvas.SetTop(selection, y);
+                selection.Width = width;
+                selection.Height = height;
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void canvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void cropButton_Click(object sender, RoutedEventArgs e)
+        {
+            CroppedImage = GetCroppedImage();
+            DialogResult = true;
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfImageViewerApp
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> imagePaths = new List<string>();
+        private int currentImageIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImage(string path)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new System.Uri(path);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+            image.Source = bitmap;
+        }
+
+        private void uploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|All files (*.*)|*.*";
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filePath = dlg.FileName;
+                imagePaths.Add(filePath);
+                currentImageIndex = imagePaths.Count - 1;
+                LoadImage(filePath);
+            }
+        }
+
+        private void previousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (imagePaths.Count == 0) return;
+            currentImageIndex--;
+            if (currentImageIndex < 0) currentImageIndex = imagePaths.Count - 1;
+            LoadImage(imagePaths[currentImageIndex]);
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (imagePaths.Count == 0) return;
+            currentImageIndex++;
+            if (currentImageIndex >= imagePaths.Count) currentImageIndex = 0;
+            LoadImage(imagePaths[currentImageIndex]);
+        }
+
+        private void cropButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (imagePaths.Count != 0)
+            {
+                CropWindow cropWindow = new CropWindow(image.Source as BitmapSource);
+                bool? result = cropWindow.ShowDialog();
+                if (result == true)
+                {
+                    image.Source = cropWindow.CroppedImage;
+                }
+            }
+        }
+    }
+}
